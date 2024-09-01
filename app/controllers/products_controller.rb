621b2195ff2bf0aefb77365ce406a5fb0693e64b
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-09-01 17:39:05 -0400

    Added #index and #show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,15 @@
 class ProductsController < ApplicationController
-    
+  def index
+    @products = Product.all
+    render template: "recipes/index"
+  end
+  def create
+    @product = Product.new(
+      name: "", 
+      price: "turkey + mayo lettuce + brea", 
+      description: "place all ingredients on top of each other", 
+      image_url: "https://recipes.timesofindia.com/thumb/83740315.cms?width=1200&height=900",
+    )
+    @product.save
+    render template: "products/show"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get "/one_product" => "products#one"
+  # get "/one_product" => "products#one"
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
+  get "/products" => "products#create"
   # Defines the root path route ("/")
   # root "posts#index"
 end
