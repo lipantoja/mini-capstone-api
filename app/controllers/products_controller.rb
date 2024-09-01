594ecf674ec276717de777427fdf317484a7e6cd
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-09-01 18:30:22 -0400

    create attempt

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,12 +4,14 @@ class ProductsController < ApplicationController
     render template: "recipes/index"
   end
   def create
+    p params[:name]
     @product = Product.new(
-      name: "", 
-      price: "turkey + mayo lettuce + brea", 
-      description: "place all ingredients on top of each other", 
-      image_url: "https://recipes.timesofindia.com/thumb/83740315.cms?width=1200&height=900",
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      image_url: params[:image_url],
     )
     @product.save
     render template: "products/show"
+  end
 end
